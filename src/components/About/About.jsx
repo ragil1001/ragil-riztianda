43c8816440a055a0ca81df391e88361ca9c8c947
ragil1001 <220711672@students.uajy.ac.id>
2025-05-16 10:20:05 +0700

    commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import ReactTypingEffect from "react-typing-effect";
+import Tilt from "react-parallax-tilt";
+import profileImage from "../../assets/ragil.jpg";
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0 md:pr-3">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Ragil Riztianda
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={[
+                "Fullstack Developer",
+                "App Developer",
+                "UI/UX Designer",
+                "Coder",
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-16 mt-12 leading-relaxed">
+            I'am an Informatics student at Universitas Atma Jaya
+            Yogyakarta—enthusiastic, proactive, and deeply passionate about both
+            Full Stack Web and Mobile App Development. I thrive on learning
+            emerging technologies and tackling challenges independently through
+            analytical and critical thinking. With a strong commitment to every
+            project I take on, my focus lies in crafting effective and
+            meaningful digital solutions across web and mobile platforms.
+          </p>
+          {/* Resume Button */}
+          <a
+            href="https://drive.google.com/file/d/1AGZIgqB495STuW1qlZ5wANvXEAZNPSAo/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Ragil Riztianda"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,286 @@
+// Skills Section Logos
+
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import phpLogo from './assets/tech_logo/php.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import dartLogo from './assets/tech_logo/dart.png';
+import flutterLogo from './assets/tech_logo/flutter.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import laravelLogo from './assets/tech_logo/laravel.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logos
+import kspLogo from './assets/company_logo/ksp_logo.png';
+import uajylogo from './assets/company_logo/uajy_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logos
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logos
+import AtmaHospital from './assets/work_logo/atmahospital.png';
+import CinemaHub from './assets/work_logo/cinemahub.png';
+import ReUseMart from './assets/work_logo/reusemart.png';
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      // { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+      { name: 'PHP', logo: phpLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      // { name: 'Node JS', logo: nodejsLogo },
+      // { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'Dart', logo: dartLogo },
+      { name: 'Flutter', logo: flutterLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Laravel', logo: laravelLogo },
+      // { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      // { name: 'Compass', logo: mcLogo },
+      // { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: kspLogo,
+      role: "Tentor Kelompok Studi Pemrograman C",
+      company: "Kelompok Studi Pemrograman",
+      date: "Jul 2023 - Jul 2024",
+      desc: "Student Mentor for introduction basic programming using C Programming Language.",
+      skills: [
+        "C",
+        // "CSS",
+        // "JavaScript",
+        // "React JS",
+        // "TypeScript",
+        // "Node JS",
+        // "Tailwind CSS",
+        // "MongoDb",
+        // "Redux",
+        // " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: uajylogo,
+      role: " Assitant Lecturer Information and Data Structure",
+      company: "Department of Informatics Atma Jaya Yogyakarta University",
+      date: "Aug 2024 - Jan 2025",
+      desc: "Assisting lecturer to conduct hands-on Information and Data Structure with C Programming Language. Educated 200+ students on Information and Data Structure concepts from basics to Stack and Binary Tree.",
+      skills: [
+        "C",
+        // "Redux",
+        // "JavaScript",
+        // "Tailwind CSS",
+        // "HTML",
+        // "CSS",
+        // "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: uajylogo,
+      role: "Assitant Lecturer Object Oriented Programming",
+      company: "Department of Informatics Atma Jaya Yogyakarta University",
+      date: "Feb 2025 - Jul 2025 (Expected)",
+      desc: "Assisting lecturer to conduct hands-on Object-Oriented Programming with Java Programming Language. Educated 200+ students on Object Oriented Programming concepts from basics to Solid Pattern and Design Pattern.",
+      skills: [
+        "Java",
+        "MySql",
+        "PhpMyAdmin",
+        // "Bootstrap",
+        // "Figma",
+        // "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: uajylogo,
+      school: "Atma Jaya Yogyakarta University",
+      date: "August 2022 - July 2026 (Expected)",
+      grade: "3.90 GPA",
+      desc: "I’m an Informatics student at Universitas Atma Jaya Yogyakarta—enthusiastic, proactive, and deeply passionate about both Full Stack Web and Mobile App Development. I thrive on learning emerging technologies and tackling challenges independently through analytical and critical thinking. With a strong commitment to every project I take on, my focus lies in crafting effective and meaningful digital solutions across web and mobile platforms.",
+      degree: "Bachelor Of Informatics - S.Kom",
+    }
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Koperasi Simpan Pinjam Java Application Desktop",
+      description:
+        "A Java-based desktop application for managing savings and loan cooperatives, built with object-oriented programming principles. This system handles complete member transactions including deposits, withdrawals, loan applications, and installment management with an informative dashboard and integrated financial reporting.",
+      image: githubdetLogo,
+      tags: ["Java", "MySql", "PhpMyAdmin"],
+      // github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+      // webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Atma Hospital Website",
+      description:
+        "A hospital management website developed with Laravel framework, MySQL database, and phpMyAdmin. The system features appointment scheduling, medical checkup registration, queue tracking, service information display, and hospital news updates, all integrated within a comprehensive healthcare management platform.",
+      image: AtmaHospital,
+      tags: ["Laravel", "CSS", "MySql", "PhpMyAdmin"],
+      // github: "https://github.com/codingmastr/CSPrep",
+      // webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "CinemaHub Application Mobile",
+      description:
+        "A Flutter-based mobile application integrated with Laravel as a backend, MySQL and phpMyAdmin for cinema ticket purchasing. This app enables users to browse movie schedules, select seats, purchase tickets, and manage bookings, providing a seamless movie-going experience directly from their mobile devices.",
+      image: CinemaHub,
+      tags: ["Flutter", "MySql", "PhpMyAdmin", "Laravel"],
+      // github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      // webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "ReUseMart Website",
+      description:
+        "A sustainable e-commerce platform built with React frontend and Laravel backend, powered by MySQL and phpMyAdmin. ReuseMart specializes in preloved items, allowing consignors to list secondhand goods while providing buyers with a seamless shopping experience for sustainable purchases through an integrated management system.",
+      image: ReUseMart,
+      tags: ["React JS", "Laravel", "MySql", "PhpMyAdmin", "Tailwind CSS"],
+      // github: "https://github.com/codingmastr/cmtk-email-validator",
+      // webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    // {
+    //   id: 4,
+    //   title: "Task Reminder Chrome Extension Tool",
+    //   description:
+    //     "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+    //   image: taskremLogo,
+    //   tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    //   github: "https://github.com/codingmastr/Task-Reminder-Tool",
+    //   webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    // },
+    // {
+    //   id: 5,
+    //   title: "ksp Digital",
+    //   description:
+    //     "The official website for ksp Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+    //   image: webverLogo,
+    //   tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    //   github: "https://github.com/codingmastr/ksp-Digital",
+    //   webapp: "https://kspdigital.com/",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
