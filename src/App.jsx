8d2b75ab7a6e71c6a2067ac655390bb12aeb18e7
Ragil Riztianda <220711672@students.uajy.ac.id>
2026-08-07 11:12:38 +0700

    design: rebuild portfolio foundation with taste-skill direction

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
-import Experience from "./components/Experience/Experience";
 import Work from "./components/Work/Work";
+import Experience from "./components/Experience/Experience";
 import Skills from "./components/Skills/Skills";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import BlurBlob from "./components/BlurBlob";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-[#050414] text-white">
-      <BlurBlob
-        position={{ top: "24%", left: "18%" }}
-        size={{ width: "28%", height: "36%" }}
-      />
-
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f20_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f20_1px,transparent_1px)] bg-[size:18px_28px] [mask-image:radial-gradient(ellipse_65%_42%_at_50%_0%,#000_70%,transparent_100%)]" />
-
-      <div className="relative pt-20">
-        <Navbar />
+    <div className="min-h-screen overflow-x-clip bg-[#f4f5f7] text-[#101216] selection:bg-[#2055ff] selection:text-white">
+      <Navbar />
+      <main>
         <About />
-        <Experience />
         <Work />
+        <Experience />
         <Skills />
         <Education />
         <Contact />
-        <Footer />
-      </div>
+      </main>
+      <Footer />
     </div>
   );
 };
