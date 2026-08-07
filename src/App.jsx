fb27f6bf3ab14c480b0780ee705ceadbc2f79be6
Ragil Riztianda <220711672@students.uajy.ac.id>
2026-08-07 09:38:34 +0700

    refocus portfolio around experience and case studies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
-import Skills from "./components/Skills/Skills";
 import Experience from "./components/Experience/Experience";
 import Work from "./components/Work/Work";
-import RepositoryCatalog from "./components/RepositoryCatalog/RepositoryCatalog";
+import Skills from "./components/Skills/Skills";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
@@ -12,21 +11,20 @@ import BlurBlob from "./components/BlurBlob";
 
 const App = () => {
   return (
-    <div className="bg-[#050414]">
+    <div className="min-h-screen bg-[#050414] text-white">
       <BlurBlob
-        position={{ top: "35%", left: "20%" }}
-        size={{ width: "30%", height: "40%" }}
+        position={{ top: "24%", left: "18%" }}
+        size={{ width: "28%", height: "36%" }}
       />
 
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f20_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f20_1px,transparent_1px)] bg-[size:18px_28px] [mask-image:radial-gradient(ellipse_65%_42%_at_50%_0%,#000_70%,transparent_100%)]" />
 
       <div className="relative pt-20">
         <Navbar />
         <About />
-        <Skills />
         <Experience />
         <Work />
-        <RepositoryCatalog />
+        <Skills />
         <Education />
         <Contact />
         <Footer />
